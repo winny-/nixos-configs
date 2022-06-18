45882a2a1d848dc9555d454caba2fb202e538fa2
Winston Weinert <winston@ml1.net>
2022-06-18 18:20:49 -0500

    laptop: enable thermald

--- common/laptop.nix
+++ common/laptop.nix
@@ -1,10 +1,13 @@
 { lib, ... }:
 {
-  services.tlp.enable = true;
-
-  # See https://linrunner.de/tlp/settings/index.html
-  services.tlp.settings = with lib; {
-    START_CHARGE_THRESH_BAT0 = mkDefault 75;
-    STOP_CHARGE_THRESH_BAT0 = mkDefault 80;
+  services.tlp = {
+    enable = true;
+    settings = with lib; {
+      # See https://linrunner.de/tlp/settings/index.html
+      START_CHARGE_THRESH_BAT0 = mkDefault 75;
+      STOP_CHARGE_THRESH_BAT0 = mkDefault 80;
+    };
   };
+
+  services.thermald.enable = true;
 }
