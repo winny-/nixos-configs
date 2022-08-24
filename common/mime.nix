9186c639498073cbb03ad8a82b1bf77d63ea4e28
Winston Weinert <git@winny.tech>
2022-08-24 17:42:00 -0500

    mime: default image viewer and fix pdf viewer

--- common/mime.nix
+++ common/mime.nix
@@ -2,10 +2,15 @@
 {
   xdg.mime.defaultApplications =
     let browser = "org.qutebrowser.qutebrowser.desktop";
+        documentViewer = "org.pwmt.zathura.desktop";
+        imageViewer = "org.xfce.ristretto.desktop";
     in {
-      "application/pdf" = "zathura.desktop";
+      "application/pdf" = documentViewer;
       "x-scheme-handler/http" = browser;
       "x-scheme-handler/https" = browser;
       "text/html" = browser;
-  };
+      "image/jpeg" = imageViewer;
+      "image/png" = imageViewer;
+      "image/gif" = imageViewer;
+    };
 }
