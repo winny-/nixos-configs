e85707df6e3094781879f4f4b5eadc1862631228
Winston Weinert <git@winny.tech>
2022-09-26 01:31:21 -0500

    flake: derive all hostnames from directory structure

--- flake.nix
+++ flake.nix
@@ -5,15 +5,21 @@
     jhmod.url = "github:sector-f/jhmod/4470eba57f3c3969ab04ef1be1dbf761be5a2c0f";
   };
 
-  outputs = { self, nixpkgs, jhmod }@inputs: {
-    nixosConfigurations =
-      nixpkgs.lib.genAttrs [ "voyager" ] (hostName:
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            (./. + "/hosts/${hostName}")
-          ];
-          specialArgs = { inherit inputs; };
-        });
-  };
+  outputs = { self, nixpkgs, jhmod }@inputs:
+    let
+      hosts = nixpkgs.lib.attrsets.mapAttrsToList
+        (name: value: name)
+        (builtins.readDir ./hosts);
+    in
+      {
+        nixosConfigurations =
+          nixpkgs.lib.genAttrs hosts (hostName:
+            nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                (./. + "/hosts/${hostName}")
+              ];
+              specialArgs = { inherit inputs; };
+            });
+      };
 }
