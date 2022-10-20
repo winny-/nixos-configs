695f7ba13e2eed6eb2e913994f345b65cfd74e6e
Winston Weinert <git@winny.tech>
2022-10-20 01:32:58 -0500

    stargate: pin kernel to fix bluetooth

--- hosts/stargate/default.nix
+++ hosts/stargate/default.nix
@@ -55,6 +55,8 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  boot.kernelPackages = pkgs.linuxPackages_5_10;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
