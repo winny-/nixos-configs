0f0b38e36aca97042694cd26a1ca7a640b87415c
Winston Weinert <git@winny.tech>
2023-01-09 09:41:57 -0600

    feat: always ship memtest86 with grub

--- common/bare-metal.nix
+++ common/bare-metal.nix
@@ -1,7 +1,8 @@
-{ lib, ... }:
+{ lib, config, ... }:
 with lib;
 {
   services.irqbalance.enable = true;
   hardware.rasdaemon.enable = mkDefault true;
   services.fwupd.enable = mkDefault true;
+  boot.loader.grub.memtest86.enable = mkDefault config.boot.loader.grub.enable;
 }
