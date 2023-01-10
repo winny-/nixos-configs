f11af2ed3f8c32268f293549161262b4099a37e3
Winston Weinert <git@winny.tech>
2023-01-09 21:50:46 -0600

    fix: add smartd by default

--- common/bare-metal.nix
+++ common/bare-metal.nix
@@ -1,7 +1,8 @@
 { lib, config, ... }:
 with lib;
 {
-  services.irqbalance.enable = true;
+  services.smartd.enable = mkDefault true;
+  services.irqbalance.enable = mkDefault true;
   hardware.rasdaemon.enable = mkDefault true;
   services.fwupd.enable = mkDefault true;
   boot.loader.grub.memtest86.enable = mkDefault config.boot.loader.grub.enable;
