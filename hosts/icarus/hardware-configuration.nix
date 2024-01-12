1b20a4f892e7af585c6e98e58731c4a89f1e55b1
Winston Weinert <git@winny.tech>
2024-01-12 06:46:54 -0600

    fix: load driver for BULIT IN laptop keyboard :X

--- hosts/icarus/hardware-configuration.nix
+++ hosts/icarus/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "ideapad_laptop" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
